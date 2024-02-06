089efde7deea2d47205de8b47a7f0e442a45a32c
RubenMartins97 <rubendevgo@gmail.com>
2024-02-06 18:38:31 -0100

    lesson 4

---
+++ lesson 4/modal-project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>{{ title }}</h1>
+  <Modal />
+</template>
+
+<script>
+import Modal from "./components/Modal.vue";
+
+export default {
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "My first vue app",
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
