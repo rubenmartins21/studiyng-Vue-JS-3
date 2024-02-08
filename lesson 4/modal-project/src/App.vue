2f91d34f01c4d95a1c1cf092ba932362db4aa433
RubenMartins97 <rubendevgo@gmail.com>
2024-02-08 16:56:57 -0100

    lesson 5

--- lesson 4/modal-project/src/App.vue
+++ lesson 4/modal-project/src/App.vue
@@ -1,6 +1,30 @@
 <template>
-  <h1>{{ title }}</h1>
-  <Modal />
+  <div>
+    <h1>{{ title }}</h1>
+    <p>Welcome...</p>
+    <div v-if="showModal">
+      <Modal theme="sale" @close="toggleModal">
+        <template v-slot:links>
+          <a href="#">sign up now</a>
+          <a href="#">more info</a>
+        </template>
+        <h1>Ninja Givaway</h1>
+        <p>Grab your ninja swag for half price</p>
+      </Modal>
+    </div>
+
+    <div v-if="showModalTwo">
+      <Modal @close="toggleModalTwo">
+        <h1>Sign up to the newsletter</h1>
+        <p>For updates and promo codes!</p>
+      </Modal>
+    </div>
+
+    <button @click="toggleModal">open modal</button>
+    <br />
+    <br />
+    <button @click="toggleModalTwo">open modal two</button>
+  </div>
 </template>
 
 <script>
@@ -12,7 +36,17 @@ export default {
   data() {
     return {
       title: "My first vue app",
+      showModal: false,
+      showModalTwo: false,
     };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo;
+    },
   },
 };
 </script>
--- lesson 4/modal-project/src/components/Modal.vue
+++ lesson 4/modal-project/src/components/Modal.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="backdrop">
-    <div class="modal">
-      <p>modal content</p>
+  <div>
+    <div class="backdrop" @click.self="closeModal">
+      <div class="modal" :class="{ sale: theme === 'sale' }">
+        <slot></slot>
+        <div class="actions">
+          <slot name="links"></slot>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: ["header", "text", "theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+  },
+};
+</script>
 
 <style>
 .modal {
@@ -21,5 +37,46 @@
   background: rgba(0, 0, 0, 0.5);
   width: 100%;
   height: 100%;
+}
+
+.modal h1 {
+  color: #03cfb4;
+  border: none;
+  padding: 0;
+}
+
+.modal p {
+  font-style: normal;
+}
+
+.modal .actions {
+  text-align: center;
+  margin: 30px 0 10px 0;
+}
+
+.modal .actions a {
+  color: #333;
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+
+.modal.sale {
+  background: crimson;
+  color: white;
+}
+
+.modal.sale h1 {
+  color: white;
+}
+
+.modal.sale .actions {
+  color: white;
+}
+
+.modal.sale .actions a {
+  color: white;
 }
 </style>
