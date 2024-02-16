77d6397499faa9bd4b0084ca4e651801ab7ee58d
RubenMartins97 <rubendevgo@gmail.com>
2024-02-16 17:10:43 -0100

    lesson 6

---
+++ lesson 6/reaction-timer/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Ninja Reaction Timer</h1>
+    <button @click="start">play</button>
+    <Block v-if="isPlaying" />
+  </div>
+</template>
+
+<script>
+import Block from "./components/Block.vue";
+export default {
+  name: "App",
+  components: {
+    Block,
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
