2c7d14dae35a168b3195be6a5329f73316ca7a16
RubenMartins97 <rubendevgo@gmail.com>
2024-03-04 15:53:08 -0100

    lesson 6

--- lesson 6/reaction-timer/src/App.vue
+++ lesson 6/reaction-timer/src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div>
     <h1>Ninja Reaction Timer</h1>
-    <button @click="start">play</button>
-    <Block v-if="isPlaying" />
+    <button @click="start" :disabled="isPlaying">play</button>
+    <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+    <Results v-if="showResults" :score="score" />
   </div>
 </template>
 
 <script>
 import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
+
 export default {
   name: "App",
   components: {
     Block,
+    Results,
   },
   data() {
     return {
       isPlaying: false,
       delay: null,
+      score: null,
+      showResults: false,
     };
   },
   methods: {
     start() {
       this.delay = 2000 + Math.random() * 5000;
       this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
     },
   },
 };
@@ -36,5 +48,21 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
